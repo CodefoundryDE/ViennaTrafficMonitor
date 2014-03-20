ddb1728d599327fa511c69cd3a06ea0a3c13a4dc
Franzi <dsfranzi@web.de>
2014-03-21 00:48:22 +0100

    CLSCompliant auf true gesetzt.

--- Vienna Traffic Monitor/Properties/AssemblyInfo.cs
+++ Vienna Traffic Monitor/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -15,6 +16,7 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
 
 // Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
 // für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
