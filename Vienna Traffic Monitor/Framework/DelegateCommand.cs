caac1706f338b9287d4b2bb464c261cb69b031a4
Franzi <dsfranzi@web.de>
2014-03-27 20:38:57 +0100

    DelegateCommand hinzugefügt (Man sollte vielleicht später noch einen Ordner dafür finden)

---
+++ Vienna Traffic Monitor/Framework/DelegateCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViennaTrafficMonitor.Framework {
+
+    public class DelegateCommand : ICommand {
+
+        private readonly Action _action;
+
+        public DelegateCommand(Action action) {
+            _action = action;
+        }
+
+        public void Execute(object parameter) {
+            _action();
+        }
+
+        public bool CanExecute(object parameter) {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
