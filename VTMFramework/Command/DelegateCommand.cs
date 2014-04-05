2e78f5682217f96970102eee2f5d51aa72710483
Franzi <dsfranzi@web.de>
2014-04-05 23:28:06 +0200

    src-Ordner im Framework entfernt.

--- VTMFramework/Command/DelegateCommand.cs
+++ VTMFramework/Command/DelegateCommand.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace VtmFramework {
+namespace VtmFramework.Command {
 
     public class DelegateCommand : ICommand {
 
