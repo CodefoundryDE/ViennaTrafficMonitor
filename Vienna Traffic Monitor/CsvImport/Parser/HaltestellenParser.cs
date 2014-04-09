54d172d32fc114e4116cf04502c773140f114683
Michael <m.hutterer@gmx.de>
2014-04-09 15:13:06 +0200

    Haltestellen Import aus CSV-Datei implementiert.
    Test geschrieben, Demo Daten im Projekt hinterlegt.
    
    Test sowie alle Tests erfolgreich.

---
+++ Vienna Traffic Monitor/CsvImport/Parser/HaltestellenParser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileHelpers;
+using FileHelpers.RunTime;
+using ViennaTrafficMonitor.Model;
+using ViennaTrafficMonitor.CsvImport.Record;
+using System.Collections.Concurrent;
+using System.Windows;
+
+namespace ViennaTrafficMonitor.CsvImport.Parser
+{
+    public static class HaltestellenParser
+    {
+
+        public static ConcurrentDictionary<int, IHaltestelle> ReadFile(String filePath)
+        {
+            FileHelperEngine<HaltestelleRecord> engine = new FileHelperEngine<HaltestelleRecord>();
+            ConcurrentDictionary<int, IHaltestelle> haltestellen = new ConcurrentDictionary<int, IHaltestelle>();
+
+            engine.ErrorManager.ErrorMode = ErrorMode.SaveAndContinue;
+
+            HaltestelleRecord[] res = engine.ReadFile(filePath);
+
+            foreach (HaltestelleRecord haltestelle in res)
+            {
+                //Übernehmen der eingelesenen Daten in das Programm-Model:
+                IHaltestelle transport = new Haltestelle();
+                Point HsLoc = new Point(haltestelle.XKoord, haltestelle.YKoord);
+
+                transport.Diva = haltestelle.Diva;
+                transport.Id = haltestelle.Id;
+                transport.Location = HsLoc;
+                transport.Name = haltestelle.Name;
+
+                //Schreiben des Models in Collection für den Rückgabewert:
+                haltestellen.AddOrUpdate(transport.Id, transport, (key, oldValue) => transport);
+            }
+            return (haltestellen);
+        }
+    }
+}
---
+++ Vienna Traffic Monitor/CsvImport/Record/HaltestelleRecord.cs
@@ -0,0 +1,100 @@
+﻿using FileHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViennaTrafficMonitor.CsvImport.Record
+{
+    [IgnoreFirst(1)]
+    [IgnoreEmptyLines()]
+    [DelimitedRecord(";")]
+    public sealed class HaltestelleRecord
+    {
+
+        private int mId;
+
+        public int Id
+        {
+            get { return mId; }
+            set { mId = value; }
+        }
+
+
+        [FieldQuoted('"', QuoteMode.OptionalForRead, MultilineMode.AllowForRead)]
+        private String mTyp;
+
+        public String Typ
+        {
+            get { return mTyp; }
+            set { mTyp = value; }
+        }
+
+
+        private int mDiva;
+
+        public int Diva
+        {
+            get { return mDiva; }
+            set { mDiva = value; }
+        }
+
+
+        [FieldQuoted('"', QuoteMode.OptionalForRead, MultilineMode.AllowForRead)]
+        private String mName;
+
+        public String Name
+        {
+            get { return mName; }
+            set { mName = value; }
+        }
+
+
+        [FieldQuoted('"', QuoteMode.OptionalForRead, MultilineMode.AllowForRead)]
+        private String mGemeinde;
+
+        public String Gemeinde
+        {
+            get { return mGemeinde; }
+            set { mGemeinde = value; }
+        }
+
+
+        private int mGemeindeId;
+
+        public int GemeindeId
+        {
+            get { return mGemeindeId; }
+            set { mGemeindeId = value; }
+        }
+
+
+        private Double mXKoord;
+
+        public Double XKoord
+        {
+            get { return mXKoord; }
+            set { mXKoord = value; }
+        }
+
+
+        private Double mYKoord;
+
+        public Double YKoord
+        {
+            get { return mYKoord; }
+            set { mYKoord = value; }
+        }
+
+
+        [FieldQuoted('"', QuoteMode.OptionalForRead, MultilineMode.AllowForRead)]
+        private String mStand;
+
+        public String Stand
+        {
+            get { return mStand; }
+            set { mStand = value; }
+        }
+    }
+}
