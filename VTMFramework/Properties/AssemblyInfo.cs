43d63fe7ed72bbf1d7f0843cb75877cab61ad977
Franzi <dsfranzi@web.de>
2014-04-16 13:57:46 +0200

    Scheduler ausgebaut

--- VTMFramework/Properties/AssemblyInfo.cs
+++ VTMFramework/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -37,3 +38,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: System.Windows.ThemeInfo(System.Windows.ResourceDictionaryLocation.None, System.Windows.ResourceDictionaryLocation.SourceAssembly)]
+[assembly: NeutralResourcesLanguageAttribute("de-DE")]
