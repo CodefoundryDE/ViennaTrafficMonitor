db3096e0391bf45bc9a7a29b33960050ff1bc920
Franzi <dsfranzi@web.de>
2014-04-22 22:42:56 +0200

    Scheduler-Test angepasst

--- VTM Framework Tests/Scheduler/SchedulerTest.cs
+++ VTM Framework Tests/Scheduler/SchedulerTest.cs
@@ -1,17 +1,18 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VtmFramework.Scheduler;
+using VtmFramework.ViewModel;
 
 namespace VtmFrameworkTests.Scheduler {
 
-    [TestClass]
+    [TestClass, System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1001:TypesThatOwnDisposableFieldsShouldBeDisposable")]
     public class SchedulerTest {
 
-        private Scheduler<object> _scheduler;
+        private Scheduler<AbstractViewModel> _scheduler;
 
         [TestInitialize]
         public void TestInitialize() {
-            _scheduler = new Scheduler<object>();
+            _scheduler = new Scheduler<AbstractViewModel>();
         }
 
         [TestMethod]
