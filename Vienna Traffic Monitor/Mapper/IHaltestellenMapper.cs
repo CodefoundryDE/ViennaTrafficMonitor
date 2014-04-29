ea5dfdf0807be007571ad369981868c890e2943d
Martin <martin.neumayer@stud.fh-rosenheim.de>
2014-04-29 17:51:29 +0200

    IHaltestellenMapper Interface hinzugefügt

--- Vienna Traffic Monitor/Mapper/IHaltestellenMapper.cs
+++ Vienna Traffic Monitor/Mapper/IHaltestellenMapper.cs
@@ -5,9 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using ViennaTrafficMonitor.Model;
 using VtmFramework.Mapper;
+using VtmFramework.Libary;
 
 namespace ViennaTrafficMonitor.Mapper {
 
-    public interface IHaltestellenMapper : IMapper<IHaltestelle> { }
+    public interface IHaltestellenMapper : IMapper<IHaltestelle> {
+
+        public List<IHaltestelle> findByName(String name);
+
+        public List<IHaltestelle> findByRectangle(Rectangle rect);
+
+        public IHaltestelle Find(int id);
+    }
 
 }
