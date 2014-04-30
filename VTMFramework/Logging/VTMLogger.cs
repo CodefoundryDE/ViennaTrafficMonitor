3848e9003607906efca26e0ea16b88fd6ceba078
Michael <m.hutterer@gmx.de>
2014-04-30 23:12:27 +0200

    Logging-Pfad geändert auf das aktuelle Arbeitsverzeichnis: Datei: VTM_Log.log
    Kein Fehler durch fehlende Schreibberechtigung festgestellt.

--- VTMFramework/Logging/VTMLogger.cs
+++ VTMFramework/Logging/VTMLogger.cs
@@ -6,31 +6,29 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
-namespace VtmFramework.Logging
-
-{
-    class VTMLogger : VtmFramework.Logging.IVtmLogger
-    {
+namespace VtmFramework.Logging {
+    class VTMLogger : VtmFramework.Logging.IVtmLogger {
         private TraceListener _tListener = null;
         private BooleanSwitch _bSwitch = null;
         private TraceSwitch _tSwitch = null;
         private static volatile VTMLogger instance;
         private static String _logPath = "";
         private static bool _logPathChanged = false;
-        private static string _defaultLogPath = Environment.ExpandEnvironmentVariables("%userprofile%");  //Environment.ExpandEnvironmentVariables("%userprofile%") + @"\AppData\VTMTrafficMonitor\VTM_Log.log";
+        private static string _defaultLogPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetModules()[0].FullyQualifiedName) + "\\VTM_LOG.log"; //Environment.ExpandEnvironmentVariables("%userprofile%") + @"\AppData\VTMTrafficMonitor\VTM_Log.log";
         private static string _traceName = "VTMTrace";
 
-        public static String LogPath
-        {
+        public static String LogPath {
             get { return _logPath; }
-            set { _logPath = value;
-                _logPathChanged = true;}
+            set {
+                _logPath = value;
+                _logPathChanged = true;
+            }
         }
 
 
-        private VTMLogger()
-        {
+        private VTMLogger() {
             _tListener = Trace.Listeners[_traceName];
             _bSwitch = new BooleanSwitch("Enable", "", VtmFramework.Properties.Settings.Default.Enable);
             _tSwitch = new TraceSwitch("Type", "", VtmFramework.Properties.Settings.Default.Type);
@@ -50,46 +48,40 @@ namespace VtmFramework.Logging
                 }
                 instance = new VTMLogger();
                 return instance;
-                }
-            } 
+            }
+        }
 
-    private static void CreateTraceListener (string logPath) {
+        private static void CreateTraceListener(string logPath) {
             Trace.Listeners.Clear();
             FileStream hlogFile = new FileStream(logPath, FileMode.OpenOrCreate, FileAccess.Write);
             TextWriterTraceListener VTMListener = new TextWriterTraceListener(hlogFile);
             VTMListener.Name = _traceName;
-            Trace.Listeners.Add(VTMListener);           
-}
+            Trace.Listeners.Add(VTMListener);
 
-        public void Error(Exception ex)
-        {
+        }
+
+        public void Error(Exception ex) {
             //Logging im Falle von "nur Errors" oder "Alles"
-            if (_bSwitch.Enabled && (_tSwitch.TraceError || _tSwitch.TraceVerbose))
-            {
+            if (_bSwitch.Enabled && (_tSwitch.TraceError || _tSwitch.TraceVerbose)) {
                 Trace.WriteLine(DateTime.Now + " " + ex.Message);
                 Trace.TraceError(ex.StackTrace);
             }
         }
 
-        public void Error (String errorMessage) {
-            if (_bSwitch.Enabled && (_tSwitch.TraceError || _tSwitch.TraceVerbose))
-            {
+        public void Error(String errorMessage) {
+            if (_bSwitch.Enabled && (_tSwitch.TraceError || _tSwitch.TraceVerbose)) {
                 Trace.TraceError(DateTime.Now + " " + errorMessage);
             }
         }
 
-        public void Warning(String warningMessage)
-        {
-            if (_bSwitch.Enabled && (_tSwitch.TraceWarning|| _tSwitch.TraceVerbose))
-            {
+        public void Warning(String warningMessage) {
+            if (_bSwitch.Enabled && (_tSwitch.TraceWarning || _tSwitch.TraceVerbose)) {
                 Trace.TraceWarning(DateTime.Now + " " + warningMessage);
             }
         }
 
-        public void Info (String infoMessage)
-        {
-            if (_bSwitch.Enabled && (_tSwitch.TraceInfo || _tSwitch.TraceVerbose))
-            {
+        public void Info(String infoMessage) {
+            if (_bSwitch.Enabled && (_tSwitch.TraceInfo || _tSwitch.TraceVerbose)) {
                 Trace.TraceInformation(DateTime.Now + " " + infoMessage);
             }
         }
