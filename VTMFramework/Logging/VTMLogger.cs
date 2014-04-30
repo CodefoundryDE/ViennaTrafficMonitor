850e424401b2226f8a36aea2eb6071e81a6986ad
Michael <m.hutterer@gmx.de>
2014-04-30 19:37:38 +0200

    Versuch das Logging auf Factory sowie variablen Log-Pfad umzustellen.
    Programm stürzt beim Ausführungsversuch nicht nachvollziehbar ab.

--- VTMFramework/Logging/VTMLogger.cs
+++ VTMFramework/Logging/VTMLogger.cs
@@ -1,25 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VtmFramework.Logging
+
 {
     class VTMLogger : VtmFramework.Logging.IVtmLogger
     {
         private TraceListener _tListener = null;
         private BooleanSwitch _bSwitch = null;
         private TraceSwitch _tSwitch = null;
+        private static volatile VTMLogger instance;
+        private static String _logPath = "";
+        private static bool _logPathChanged = false;
+        private static string _defaultLogPath = Environment.ExpandEnvironmentVariables("%userprofile%");  //Environment.ExpandEnvironmentVariables("%userprofile%") + @"\AppData\VTMTrafficMonitor\VTM_Log.log";
+        private static string _traceName = "VTMTrace";
 
-        public VTMLogger()
+        public static String LogPath
         {
-            _tListener = Trace.Listeners["Tracer"];
+            get { return _logPath; }
+            set { _logPath = value;
+                _logPathChanged = true;}
+        }
+
+
+        private VTMLogger()
+        {
+            _tListener = Trace.Listeners[_traceName];
             _bSwitch = new BooleanSwitch("Enable", "", VtmFramework.Properties.Settings.Default.Enable);
             _tSwitch = new TraceSwitch("Type", "", VtmFramework.Properties.Settings.Default.Type);
 
         }
+
+        public static VTMLogger Instance {
+            [MethodImpl(MethodImplOptions.Synchronized)]
+            get {
+                if (_logPathChanged == true) {
+                    CreateTraceListener(_logPath);
+                }
+                if (instance == null) {
+                    CreateTraceListener(_defaultLogPath);
+                } else {
+                    return instance;
+                }
+                instance = new VTMLogger();
+                return instance;
+                }
+            } 
+
+    private static void CreateTraceListener (string logPath) {
+            Trace.Listeners.Clear();
+            FileStream hlogFile = new FileStream(logPath, FileMode.OpenOrCreate, FileAccess.Write);
+            TextWriterTraceListener VTMListener = new TextWriterTraceListener(hlogFile);
+            VTMListener.Name = _traceName;
+            Trace.Listeners.Add(VTMListener);           
+}
 
         public void Error(Exception ex)
         {
---
+++ VTMFramework/Logging/VTMLoggerFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VtmFramework.Logging {
+    public static class VTMLoggerFactory {
+
+        public static IVtmLogger getInstance (string logPath) {
+            VTMLogger.LogPath = logPath;
+            return getInstance();
+        }
+
+        public static IVtmLogger getInstance () {
+            return VTMLogger.Instance;
+        }
+
+    }
+}
