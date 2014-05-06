974159b5aa906303b0bba21dc26b04117b26da94
Michael <m.hutterer@gmx.de>
2014-05-06 10:55:31 +0200

    Anlage und Implementierung des RequesterProxy.cs sowie der entsprechenden VtmResponse.cs

---
+++ Vienna Traffic Monitor/Deserializer/RequesterProxy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViennaTrafficMonitor.Deserializer {
+    public static class RblRequesterProxy {
+
+        public static async Task<IList<VtmResponse>> GetProxyResponseAsync(int rbl) {
+            
+            Response response = await RblRequester.GetResponseAsync(rbl);
+            IList<VtmResponse> responses = new List<VtmResponse>();
+            foreach (Monitor monitor in response.Data.Monitors) {
+                foreach (Line line in monitor.Lines) {
+                    foreach (Departure departure in line.Departures.Departure) {
+                        responses.Add(new VtmResponse(line, departure, response.Message, monitor.LocationStop));
+                    }
+                }
+            }
+            return responses;
+        }
+
+    }
+}
---
+++ Vienna Traffic Monitor/Deserializer/VtmResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViennaTrafficMonitor.Deserializer {
+    public class VtmResponse {
+
+        public readonly Line Line { get; set; }
+
+        public readonly Departure Departure { get; set; }
+
+        public readonly TrafficInfoCategory TrafficInfoCategory {get; set;}
+
+        public readonly TrafficInfoCategoryGroup TrafficInfoCategoryGroup { get; set; }
+
+        public readonly LocationStop LocationStop { get; set; }
+
+        public VtmResponse(Line line, Departure departure, Message message, LocationStop locstop, TrafficInfoCategory tic, TrafficInfoCategoryGroup ticg) {
+            Line = line;
+            Departure = departure;
+            LocationStop = locstop;
+            TrafficInfoCategory = tic;
+            TrafficInfoCategoryGroup = ticg;
+        }
+    }
+}
