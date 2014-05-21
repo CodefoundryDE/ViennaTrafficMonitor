7f8da18fab814b1eafadc22e0a9c3ef215af3f33
Franzi <info@codefoundry.de>
2014-05-21 12:05:52 +0200

    Bugfix im StringToTimeConverter

--- VTMFramework/Converter/StringToTimeConverter.cs
+++ VTMFramework/Converter/StringToTimeConverter.cs
@@ -12,7 +12,7 @@ namespace VtmFramework.Converter {
     public class StringToTimeConverter : IValueConverter {
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-            DateTime datetime = DateTime.ParseExact(value as string, "yyyy-MM-dd'T'HH:mm:ss.fffzz'00'", CultureInfo.InvariantCulture);
+            DateTime datetime = DateTime.ParseExact((string)value, "yyyy-MM-dd'T'HH:mm:ss.fffzz'00'", CultureInfo.InvariantCulture);
             return datetime.ToString("HH:mm");
         }
 
