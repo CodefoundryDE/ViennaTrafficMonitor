b36a06483258160557a672363b7d98606549e783
Franzi <info@codefoundry.de>
2014-06-13 19:14:41 +0200

    Die Anwendung lässt sich jetzt auf einen anderen Monitor legen.

--- Vienna Traffic Monitor/View/MainWindow.xaml.cs
+++ Vienna Traffic Monitor/View/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
@@ -28,7 +29,25 @@ namespace ViennaTrafficMonitor.View {
         }
 
         private void OnGuiLoaded(object sender, RoutedEventArgs e) {
+            _setMonitor();
             ((MainViewModel)this.DataContext).initializeApp();
+        }
+
+        private void _setMonitor() {
+            Screen screen;
+            try {
+                screen = Screen.AllScreens[Properties.Settings.Default.Monitor];
+            } catch (IndexOutOfRangeException) {
+                // Ausweichen auf den Hauptbildschirm
+                screen = Screen.PrimaryScreen;
+                Properties.Settings.Default.Monitor = 0;
+                Properties.Settings.Default.Save();
+            }
+            this.WindowState = WindowState.Normal;
+            this.Top = screen.WorkingArea.Top;
+            this.Left = screen.WorkingArea.Left;
+            this.WindowState = WindowState.Maximized;
+            this.WindowStyle = WindowStyle.None;
         }
 
     }
