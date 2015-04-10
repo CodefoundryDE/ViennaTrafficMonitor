2ada1fafd2d2e76d682dc484ca79d930cd3187d0
Franz Wimmer <info@codefoundry.de>
2015-04-10 14:52:30 +0200

    AbstractViewModel aufgeräumt

--- VTMFramework/ViewModel/AbstractViewModel.cs
+++ VTMFramework/ViewModel/AbstractViewModel.cs
@@ -11,11 +11,11 @@ namespace VtmFramework.ViewModel {
 
     public abstract class AbstractViewModel : IObserver<EErrorResult>, IDisposable, INotifyPropertyChanged {
 
-        private bool _disposed = false;
+        private bool _disposed;
 
         public event EventHandler<ErrorEventArgs> ErrorRaised;
         public event EventHandler ErrorCleared;
-        private AutoResetEvent _errorCompleted;
+        private readonly AutoResetEvent _errorCompleted;
         private EErrorResult _errorResult;
 
         //public ErrorViewModel Error { get; private set; }
@@ -31,30 +31,30 @@ namespace VtmFramework.ViewModel {
         }
 
         protected async Task<EErrorResult> RaiseError(string title, string message, EErrorButtons buttonSet) {
-            ErrorViewModel Error = ErrorViewModelFactory.GetInstance(title, message, buttonSet, this);
-            RaiseErrorEvent(Error);
-            EErrorResult result = await _raiseError();
+            var error = ErrorViewModelFactory.GetInstance(title, message, buttonSet, this);
+            RaiseErrorEvent(error);
+            var result = await _raiseError();
             ClearErrorEvent();
             return result;
         }
 
         protected async Task<EErrorResult> RaiseError(string title, string message, EErrorButtons buttonSet, Exception ex) {
-            ErrorViewModel Error = ErrorViewModelFactory.GetInstance(title, message, buttonSet, this, ex);
-            RaiseErrorEvent(Error);
-            EErrorResult result = await _raiseError();
+            var error = ErrorViewModelFactory.GetInstance(title, message, buttonSet, this, ex);
+            RaiseErrorEvent(error);
+            var result = await _raiseError();
             ClearErrorEvent();
             return result;
         }
 
         private void RaiseErrorEvent(ErrorViewModel error) {
-            EventHandler<ErrorEventArgs> handler = ErrorRaised;
+            var handler = ErrorRaised;
             if (handler != null) {
                 handler(this, new ErrorEventArgs(error));
             }
         }
 
         private void ClearErrorEvent() {
-            EventHandler handler = ErrorCleared;
+            var handler = ErrorCleared;
             if (handler != null) {
                 handler(this, new EventArgs());
             }
@@ -73,24 +73,6 @@ namespace VtmFramework.ViewModel {
             if (handler != null)
                 handler(this, new PropertyChangedEventArgs(propertyName));
         }
-
-        ///// <summary>
-        ///// Zeigt an, ob der Scheduler das ViewModel wechseln darf.
-        ///// </summary>
-        ///// <returns></returns>
-        //private bool _canSwitch;
-        //public bool CanSwitch {
-        //    get {
-        //        lock (syncRoot) {
-        //            return (Error == null) && _canSwitch;
-        //        }
-        //    }
-        //    set {
-        //        lock (syncRoot) {
-        //            _canSwitch = value;
-        //        }
-        //    }
-        //}
 
         #region Observer
         public void OnCompleted() {
@@ -113,13 +95,12 @@ namespace VtmFramework.ViewModel {
         }
 
         protected virtual void Dispose(bool disposing) {
-            if (!_disposed) {
-                if (disposing) {
-                    _errorCompleted.Set();
-                    _errorCompleted.Dispose();
-                }
-                _disposed = true;
+            if (_disposed) return;
+            if (disposing) {
+                _errorCompleted.Set();
+                _errorCompleted.Dispose();
             }
+            _disposed = true;
         }
 
         ~AbstractViewModel() {
